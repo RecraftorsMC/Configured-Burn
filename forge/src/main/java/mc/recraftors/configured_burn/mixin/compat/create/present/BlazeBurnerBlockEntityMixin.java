c9c65e42315d935c658cf6be3aaa2beb6bda7f70
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-02-21 19:14:25 +0100

    Fix blaze burner compat mixin (with identical code, I blame Forge mixins & stupid refmaps)

--- forge/src/main/java/mc/recraftors/configured_burn/mixin/compat/create/present/BlazeBurnerBlockEntityMixin.java
+++ forge/src/main/java/mc/recraftors/configured_burn/mixin/compat/create/present/BlazeBurnerBlockEntityMixin.java
@@ -7,28 +7,32 @@ import mc.recraftors.configured_burn.ConfiguredBurnTime;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Optional;
 
-@Mixin(value = BlazeBurnerBlockEntity.class, remap = false)
+@Mixin(value = BlazeBurnerBlockEntity.class)
 public abstract class BlazeBurnerBlockEntityMixin {
-    @Redirect(method = "tryUpdateFuel", at = @At(value = "CONSTANT", args = "intValue=3200"))
-    private int customOverheatTimeRedirect(ItemStack itemStack, boolean forceOverflow, boolean simulate) {
+    @ModifyConstant(method = "tryUpdateFuel", constant = @Constant(intValue = 3200), remap = false)
+    private int customOverheatTimeInjector(int constant, ItemStack itemStack) {
         return ConfiguredBurnTime.getFuelEntry(itemStack).map(entry -> {
             BurnRecipe.CompatBurnTime compat = entry.compat();
-            if (compat == BurnRecipe.CompatBurnTime.DEFAULT || compat.getCreateOverheatTime() < 0) return 3200;
+            if (compat.getCreateOverheatTime() < 0) return 3200;
             return compat.getCreateOverheatTime();
         }).orElse(3200);
     }
 
-    @Redirect(method = "tryUpdateFuel", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/AllTags$AllItemTags;matches(Lnet/minecraft/item/ItemStack;)Z", opcode = 0))
+    @Redirect(method = "tryUpdateFuel", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/AllTags$AllItemTags;matches(Lnet/minecraft/item/ItemStack;)Z", ordinal = 0))
     private boolean doesConfiguredOverheatInjector(AllTags.AllItemTags instance, ItemStack stack){
-        Optional<ConfiguredBurnTime.CustomFuelEntry> entry = ConfiguredBurnTime.getFuelEntry(stack);
-        if (entry.isPresent()) {
-            BurnRecipe.CompatBurnTime compat = entry.get().compat();
-            if (compat != BurnRecipe.CompatBurnTime.DEFAULT && compat.getCreateOverheatTime() >= 0) {
-                return compat.getCreateOverheatTime() > 0;
+        if (instance == AllTags.AllItemTags.BLAZE_BURNER_FUEL_SPECIAL) {
+            Optional<ConfiguredBurnTime.CustomFuelEntry> entry = ConfiguredBurnTime.getFuelEntry(stack);
+            if (entry.isPresent()) {
+                BurnRecipe.CompatBurnTime compat = entry.get().compat();
+                if (compat != BurnRecipe.CompatBurnTime.DEFAULT && compat.getCreateOverheatTime() >= 0) {
+                    return compat.getCreateOverheatTime() > 0;
+                }
             }
         }
         return instance.matches(stack);
